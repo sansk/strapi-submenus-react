71705eef1824aceec981b8fef3ab38519c33b376
sansk <iamsangyk@gmail.com>
2023-06-05 17:02:04 +0530

    update

---
+++ src/Submenu.jsx
@@ -0,0 +1,37 @@
+import { useGlobalContext } from "./Context";
+import sublinks from "./data";
+import { useRef } from "react";
+
+export const Submenu = () => {
+  const { pageId, setPageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+
+  const submenuContainer = useRef(null);
+  const handleMouseLeave = (event) => {
+    setPageId(null);
+  };
+
+  return (
+    <div
+      className={currentPage ? "submenu show-submenu" : "submenu"}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}>
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a href={url} key={id}>
+              {icon} {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
